94aa0e867cbf16625851fa658c3eec104611dec3
Sergio Campama <sergiocampama@gmail.com>
2011-06-23 02:19:39 -0400

    Reindenting

--- TimerBlink/src/timerblink.c
+++ TimerBlink/src/timerblink.c
@@ -21,37 +21,37 @@
 void timer_init()
 {
 	//Select ACLK, TimerA counter divisor in 1
-    TACTL |= TASSEL_1 + ID_DIV1; 
+	TACTL |= TASSEL_1 + ID_DIV1; 
 
-    //Enables the CCR0 interrupt
+	//Enables the CCR0 interrupt
 	TACCTL0 |= CCIE; 
 }
 
 void led_init()
 {
 	//Set the direction as output
-    LED_DIR |= LED_RED; 
-    LED_DIR |= LED_GREEN;
+	LED_DIR |= LED_RED; 
+	LED_DIR |= LED_GREEN;
 }
 
 void init()
 {
 	//Stop the watchdog
-    WDTCTL = WDTPW + WDTHOLD; 
-	
+	WDTCTL = WDTPW + WDTHOLD; 
+
 	//Inits the different parts
-    led_init();
+	led_init();
 	timer_init();
-	
-    //Enable global interrupts
-    _BIS_SR(GIE);
+
+	//Enable global interrupts
+	_BIS_SR(GIE);
 
 }
 
 void timer_start()
 {
 	TACTL |= MC_1;//Sets the TimerA count mode in UP, and thus starting the Timer
-	
+
 	//Since the crystal attached to LFXT is 32.768 kHz, 
 	//when the counter reaches this value, exactly 1 second should have passed
 	//This could be off a little because of the clock skew (20 ppm)
@@ -63,7 +63,7 @@ int main()
 {
 	init();
 	timer_start();
-	
+
 	//We enter the LPM3 Low Power Mode, which only leaves ACLK running
 	//This way, the CPU will be off for 1 second, toggle the LED, and turn off for another second
 	_BIS_SR(LPM3_bits);
@@ -73,6 +73,6 @@ int main()
 
 //CCR0 timer interrupt, which toggles the LEDs
 interrupt(TIMERA0_VECTOR) TIMERA0_ISR() {
-    LED_OUT ^= LED_GREEN;
+	LED_OUT ^= LED_GREEN;
 	LED_OUT ^= LED_RED;
 }
