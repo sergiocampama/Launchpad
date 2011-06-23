99e3dd9d3712171757e137ba990a6114e6d6b404
Sergio Campama <sergiocampama@gmail.com>
2011-06-23 14:54:33 -0400

    Fixing the button project

--- ButtonBlink/src/buttonblink.c
+++ ButtonBlink/src/buttonblink.c
@@ -16,25 +16,31 @@
 
 //Interrupt Enable
 #define BUTTON_IE P1IE
+#define BUTTON_IES P1IES
 #define BUTTON_IFG P1IFG
 
 //P1.0 and P1.6 are the RED and GREEN LEDs respectively
 #define LED_RED BIT0
 #define LED_GREEN BIT6
 
-//P1.2 is the button
-#define BUTTON BIT2
+//P1.3 is the button
+#define BUTTON BIT3
 
 void button_init()
 {
 	BUTTON_IE |= BUTTON; //We activate the button interrupt
+	BUTTON_IES |= BUTTON; //High to low edge
+	BUTTON_IFG &= ~BUTTON;
 }
 
 void led_init()
 {
 	//Set the direction as output
-	LED_DIR |= LED_RED; 
-	LED_DIR |= LED_GREEN;
+	LED_DIR |= LED_RED + LED_GREEN; 
+	
+	//Start with the red led on.
+	LED_OUT &= ~LED_GREEN;
+	LED_OUT |= LED_RED;
 }
 
 void init()
@@ -59,16 +65,17 @@ int main()
 	//We enter the LPM4 Low Power Mode, which leaves nothing running
 	//This way, the CPU will be off until the button is pressed
 	_BIS_SR(LPM4_bits);
-	
+
 	return 0;
 }
 
 //Port 1 interrupt, which toggles the LEDs
 interrupt(PORT1_VECTOR) PORT1_ISR() {
+	
 	if (BUTTON & BUTTON_IFG)
 	{
-		LED_OUT ^= LED_GREEN;
-		LED_OUT ^= ~LED_RED;
+		LED_OUT ^= LED_GREEN + LED_RED;
 		BUTTON_IFG &= ~BUTTON;
 	}
+	
 }
